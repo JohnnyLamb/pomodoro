73e5ee11d4cbc32319c3287d3d35d4ae29f5375c
johnny lamb <agnusjack@gmail.com>
2015-09-01 17:17:36 -0600

    working with flipclock library

--- js/main.js
+++ js/main.js
@@ -1,20 +1,11 @@
-// add scripts
-
 $(document).on('ready', function() {
       $("#start").on("click", function() {
-
-
         var clock = $('.your-clock').FlipClock({
         });
-
         clock.setCountdown(true);
         clock.setTime(1499);
         console.log('hey');
-
       });
-
        $("#stop").on("click", function() {
-
       });
-
 });
